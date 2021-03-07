b1b2ab5728cdcf1176a2c1263e694aa19d9114a9
Leandro Lima <leandro.lima@oowlish.com>
2021-03-07 15:32:30 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'be216518ced106c6419a45ced75ed108'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fcd3c6d-d8c5-4032-81ff-187b335004ce'
+};
