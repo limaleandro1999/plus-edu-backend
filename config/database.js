455e37d4f0e522a5b958d3e429381dda0ec5528d
Leandro lima <leandro.lima@oowlish.com>
2021-04-01 11:14:34 -0300

    logic to get database url connection

--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+const hasConnectionURL = !!process.env.DATABASE_URL;
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -5,11 +9,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'plus-edu'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'root'),
+        host: hasConnectionURL ? config.host : env('DATABASE_HOST', '127.0.0.1'),
+        port: hasConnectionURL ? config.port : env.int('DATABASE_PORT', 5432),
+        database: hasConnectionURL ? config.database : env('DATABASE_NAME', 'plus-edu'),
+        username: hasConnectionURL ? config.user : env('DATABASE_USERNAME', 'postgres'),
+        password: hasConnectionURL ? config.password : env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
