89342950aa39f0c73295104240b64b466b8104e3
Leandro lima <leandro.lima@oowlish.com>
2021-04-01 14:45:20 -0300

    logic to add answers to a question

--- api/answer/controllers/answer.js
+++ api/answer/controllers/answer.js
@@ -5,6 +5,8 @@ module.exports = {
   async create(ctx) {
     const { id } = ctx.req.user;
     const answer = await strapi.services.answer.create({ ...ctx.request.body, users_permissions_user: id });
+    const question = await strapi.query('question').findOne({ id: answer.question.id });
+    await strapi.query('question').update({ id: answer.question.id }, { answers: [ ...question.answers, answer.id ] });
     return sanitizeEntity(answer, { model: strapi.models.answer });
   }
 };
